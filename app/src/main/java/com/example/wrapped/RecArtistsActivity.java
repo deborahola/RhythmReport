8e5fc42dd2a151fa9b54ea08bc519cb08055abce
Deborah Oladokun <deboraholadokun2003@gmail.com>
2024-04-21 17:31:48 -0400

    UI change for rec artists

--- app/src/main/java/com/example/wrapped/RecArtistsActivity.java
+++ app/src/main/java/com/example/wrapped/RecArtistsActivity.java
@@ -2,12 +2,14 @@ package com.example.wrapped;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -36,6 +38,42 @@ public class RecArtistsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rec_artists);
 
+        // Animated gradient background that can change from one color to another
+        LinearLayout cardRecArtist1 = findViewById(R.id.cardRecArtist1);
+        AnimationDrawable animationDrawable1 = (AnimationDrawable) cardRecArtist1.getBackground();
+        animationDrawable1.setEnterFadeDuration(1000);
+        animationDrawable1.setExitFadeDuration(1000);
+        animationDrawable1.start();
+
+        // Animated gradient background that can change from one color to another
+        LinearLayout cardRecArtist2 = findViewById(R.id.cardRecArtist2);
+        AnimationDrawable animationDrawable2 = (AnimationDrawable) cardRecArtist2.getBackground();
+        animationDrawable2.setEnterFadeDuration(1000);
+        animationDrawable2.setExitFadeDuration(1000);
+        animationDrawable2.start();
+
+        // Animated gradient background that can change from one color to another
+        LinearLayout cardRecArtist3 = findViewById(R.id.cardRecArtist3);
+        AnimationDrawable animationDrawable3 = (AnimationDrawable) cardRecArtist3.getBackground();
+        animationDrawable3.setEnterFadeDuration(1000);
+        animationDrawable3.setExitFadeDuration(1000);
+        animationDrawable3.start();
+
+        // Animated gradient background that can change from one color to another
+        LinearLayout cardRecArtist4 = findViewById(R.id.cardRecArtist4);
+        AnimationDrawable animationDrawable4 = (AnimationDrawable) cardRecArtist4.getBackground();
+        animationDrawable4.setEnterFadeDuration(1000);
+        animationDrawable4.setExitFadeDuration(1000);
+        animationDrawable4.start();
+
+        // Animated gradient background that can change from one color to another
+        LinearLayout cardRecArtist5 = findViewById(R.id.cardRecArtist5);
+        AnimationDrawable animationDrawable5 = (AnimationDrawable) cardRecArtist5.getBackground();
+        animationDrawable5.setEnterFadeDuration(1000);
+        animationDrawable5.setExitFadeDuration(1000);
+        animationDrawable5.start();
+
+
         String[] topArtists = getIntent().getStringArrayExtra("topArtists");
         String[] topArtistPics = getIntent().getStringArrayExtra("topArtistPics");
         String[] recArtists = getIntent().getStringArrayExtra("recArtists");
@@ -47,7 +85,7 @@ public class RecArtistsActivity extends AppCompatActivity {
 
         playTrack(trackURIs[3]);
 
-        TextView recArtistsTextView = findViewById(R.id.rec_artists_text_view);
+        //TextView recArtistsTextView = findViewById(R.id.rec_artists_text_view);
         Button toGenreBtn = findViewById(R.id.to_genre_btn);
 
         toGenreBtn.setOnClickListener(new View.OnClickListener() {
@@ -65,12 +103,34 @@ public class RecArtistsActivity extends AppCompatActivity {
             }
         });
 
+        TextView recArtist1 = findViewById(R.id.recArtist1);
+        TextView recArtist2 = findViewById(R.id.recArtist2);
+        TextView recArtist3 = findViewById(R.id.recArtist3);
+        TextView recArtist4 = findViewById(R.id.recArtist4);
+        TextView recArtist5 = findViewById(R.id.recArtist5);
+
         if (recArtists != null) {
             String joinedRecArtists = TextUtils.join(", ", recArtists);
-            recArtistsTextView.setText(joinedRecArtists);
+            //recArtistsTextView.setText(joinedRecArtists);
+
+            String[] words = joinedRecArtists.split(", ");
+
+            recArtist1.setText("#1  " + words[0]);
+            recArtist2.setText("#2  " + words[1]);
+            recArtist3.setText("#3  " + words[2]);
+            recArtist4.setText("#4  " + words[3]);
+            recArtist5.setText("#5  " + words[4]);
+
         } else {
             String ans="No recommended artists data available";
-            recArtistsTextView.setText(ans);
+            //recArtistsTextView.setText(ans);
+
+            recArtist1.setText(ans);
+            recArtist2.setText(ans);
+            recArtist3.setText(ans);
+            recArtist4.setText(ans);
+            recArtist5.setText(ans);
+
         }
     }
 
